517028d5fc5242bff6e6dcbc5879405847df518b
bnt10 <43377349+bnt10@users.noreply.github.com>
2022-09-18 08:31:13 +0000

    add chapter03  - sequential-search

--- chapter02/tower-of-hanoi.py
+++ chapter02/tower-of-hanoi.py
@@ -1,14 +1,23 @@
-def hanoi(n, a, b):
+def hanoi(n, frm, to):
     if n > 1:
-        hanoi(n - 1, a, 6 - a - b)  # 기둥이 1개 이상이면 그룹으로 묶인 n-1개 원판을
+        hanoi(n - 1, frm, 6 - frm - to)  # 기둥이 1개 이상이면 그룹으로 묶인 n-1개 원판을
         # 중간으로 먼저 다 옮긴다
-    print(a, b)
+
+    print(frm, to)
 
     if n > 1:
-        hanoi(n - 1, 6 - a - b, b)
+        hanoi(n - 1, 6 - frm - to, to)
 
 
 n = int(input())
 
 print(2**n - 1)  #총 이동해야 하는 횟수
 hanoi(n, 1, 3)
+
+# n == 1
+# 1 -> 3  = 1 번
+
+# n == 2
+# 1 -> 2  = 1번
+# 1 -> 3  = 2번
+# 2 -> 3  = 3번
---
+++ chapter03/sequential-search.py
@@ -0,0 +1,22 @@
+# 순차검색 예제
+
+
+def sequentialSearch(numberList, targetValue):
+
+    # 입력: numberList:숫자 리스트, targetValue: 찾는값
+
+    # 출력: numberList에서  targetValue 의 index
+ㄴ
+    for index in range(0, len(numberList)):  #전체 리스트를 순차적으로 비교
+        if targetValue == numberList[index]:
+
+            return print(f'targetValue의 Index {index}')  # targetValue의 index
+
+    return print('해당값은 존재하지 않습니다.')  #값이 없는 경우
+
+
+numList = [1, 2, 3, 4, 5]
+
+sequentialSearch(numList, 3)
+
+sequentialSearch(numList, 10)
