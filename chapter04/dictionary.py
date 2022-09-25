3bca93f5b0c7b0a86f80a91178aaf442ae1aaad6
bnt10 <43377349+bnt10@users.noreply.github.com>
2022-09-25 10:16:46 +0000

    add chapter04  - dictionary

--- chapter04/dictionary.py
+++ chapter04/dictionary.py
@@ -1,1 +1,33 @@
+"""
+given :
+  studentDict = {
+      39: 'Justin',
+      14: 'John',
+      67: 'Mike',
+      105: 'Summer',
+  }
 
+when :
+  print(findNameById(39))  
+
+then : 
+  key가 존재하는경우 return true
+  그렇지 않으면 false
+"""
+studentDict = {
+    39: 'Justin',
+    14: 'John',
+    67: 'Mike',
+    105: 'Summer',
+}
+
+
+def findNameById(key):
+    if key in studentDict:
+        return True
+
+    return False
+
+
+#print(findNameById(39)) # true
+print(findNameById(20))  # false
