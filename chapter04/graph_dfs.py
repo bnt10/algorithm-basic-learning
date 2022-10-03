c43e9003839193872023bc5a0183794c899614e6
bnt10 <43377349+bnt10@users.noreply.github.com>
2022-10-03 08:28:11 +0000

    Fix chapter04 Bfs  modify class name

--- chapter04/graph_dfs.py
+++ chapter04/graph_dfs.py
@@ -5,26 +5,27 @@ given:
 
 when:
   V = 6
-  bfsGraph = BfsWithAdjList(V)
-  bfsGraph.addEdge(0, 2)
-  bfsGraph.addEdge(0, 3)
-  bfsGraph.addEdge(1, 2)
-  bfsGraph.addEdge(1, 3)
-  bfsGraph.addEdge(1, 4)
-  bfsGraph.addEdge(2, 0)
-  bfsGraph.addEdge(2, 4)
-  bfsGraph.addEdge(3, 0)
-  bfsGraph.addEdge(4, 1)
-  bfsGraph.addEdge(4, 5)
-  bfsGraph.addEdge(5, 2)
-  bfsGraph.addEdge(5, 4)
-  print(bfsGraph.display(1))
+  dfsGraph = dfsWithAdjList(V)
+  dfsGraph.addEdge(0, 2)
+  dfsGraph.addEdge(0, 3)
+  dfsGraph.addEdge(1, 2)
+  dfsGraph.addEdge(1, 3)
+  dfsGraph.addEdge(1, 4)
+  dfsGraph.addEdge(2, 0)
+  dfsGraph.addEdge(2, 4)
+  dfsGraph.addEdge(3, 0)
+  dfsGraph.addEdge(4, 1)
+  dfsGraph.addEdge(4, 5)
+  dfsGraph.addEdge(5, 2)
+  dfsGraph.addEdge(5, 4)
+  print(dfsGraph.display(1))
+
   
 then:
  [1, 4, 5, 2, 0, 3]
 '''
 
-class BfsWithAdjList():
+class dfsWithAdjList():
 
     def __init__(self, V):
         self.vertex = V
@@ -47,18 +48,17 @@ class BfsWithAdjList():
 
 
 V = 6
-bfsGraph = BfsWithAdjList(V)
-bfsGraph.addEdge(0, 2)
-bfsGraph.addEdge(0, 3)
-bfsGraph.addEdge(1, 2)
-bfsGraph.addEdge(1, 3)
-bfsGraph.addEdge(1, 4)
-bfsGraph.addEdge(2, 0)
-bfsGraph.addEdge(2, 4)
-bfsGraph.addEdge(3, 0)
-bfsGraph.addEdge(4, 1)
-bfsGraph.addEdge(4, 5)
-bfsGraph.addEdge(5, 2)
-bfsGraph.addEdge(5, 4)
-print(bfsGraph.display(1))
-
+dfsGraph = dfsWithAdjList(V)
+dfsGraph.addEdge(0, 2)
+dfsGraph.addEdge(0, 3)
+dfsGraph.addEdge(1, 2)
+dfsGraph.addEdge(1, 3)
+dfsGraph.addEdge(1, 4)
+dfsGraph.addEdge(2, 0)
+dfsGraph.addEdge(2, 4)
+dfsGraph.addEdge(3, 0)
+dfsGraph.addEdge(4, 1)
+dfsGraph.addEdge(4, 5)
+dfsGraph.addEdge(5, 2)
+dfsGraph.addEdge(5, 4)
+print(dfsGraph.display(1))
