40df8f66b570ca5e008cbc8f6cf4e10f408010da
bnt10 <43377349+bnt10@users.noreply.github.com>
2022-10-30 10:26:48 +0000

    Add solving

---
+++ solving/wil-be-average-4344.py
@@ -0,0 +1,15 @@
+inputRaw = int(input())
+
+for _ in range(inputRaw):
+
+    data = scoreList = list(map(int, input().split()))
+    personnel = data[0]
+
+    sum_value = sum(i for i in data[1:])
+    avg = sum_value / personnel
+    count = 0
+    for i in data[1:]:
+        if i > avg:
+            count += 1
+
+    print("%.3f%%" % (count / personnel * 100))
